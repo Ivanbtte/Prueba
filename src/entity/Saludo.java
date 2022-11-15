b1aae0fb52b99c5b18fb52b16372a3e7e15ee90b
Esttrella <lpzlpz2022@gmail.com>
2022-11-15 17:54:24 -0600

    Agregando el metodo tres

--- src/entity/Saludo.java
+++ src/entity/Saludo.java
@@ -10,6 +10,8 @@ public class Saludo {
          public void metodo2(){
             System.out.println("Soy el DOS");
         }
-    
+        public void metodo3(){
+            System.out.println("Soy el TRES");
+        }
     
 }
